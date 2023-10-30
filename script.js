b3c97026a367e01e3a25f894939c6afd8c4d9589
ImcalledIshita <95135379+ImcalledIshita@users.noreply.github.com>
2023-10-30 14:16:24 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const wrapper = document.querySelector(".container"),
-generate8tn = wrapper.querySelector(".form button");
+const container = document.querySelector(".container"),
+qrInput = container.querySelector(".form input"),
+generate8tn = container.querySelector(".form button"),
+qrImg = container.querySelector(".qr-code img");
+
 
 generate8tn.addEventListener("click", () => {
-    wrapper.classList.add("active");
+    let qrValue = qrInput.value;
+    if (!qrValue) return; //if input is empty then return
+    generate8tn.innerHTML = "Generating Qr Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        container.classList.add("active");
+        generate8tn.innerHTML = "Generate Qr Code";
+    });
+   
 });
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value){
+        container.classList.remove("active");
+    }
+})

3c6fcbffad700caa1f788d351325dd006f036a4b
ImcalledIshita <95135379+ImcalledIshita@users.noreply.github.com>
2023-10-30 13:44:24 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".container"),
+generate8tn = wrapper.querySelector(".form button");
+
+generate8tn.addEventListener("click", () => {
+    wrapper.classList.add("active");
+});
